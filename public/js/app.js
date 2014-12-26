ce5637df5e002c83d766881591e776fa82dc29e8
Sid Cho <awsidc@gmail.com>
2014-12-25 22:19:26 -0500

    xmas-ing alright

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,13 @@
 window.onload = function() {	
+	dropObjects = [];
+	dragObjects = [];
+	console.log(dragObjects.length);
 	addDropBehavior();
 	addDragBehavior();
 };
 
+var dropObjects = [];
+var dragObjects = [];
 var addDragBehavior = function(){
 	var cols = document.querySelectorAll("#left_col");	
 	[].forEach.call(cols, function(col) { 	
@@ -25,18 +30,29 @@ var addDragBehavior = function(){
 			},false);
 
 		col.addEventListener("dragover", function(event)
-				{	
+				{		
+					var position = col.getAttribute('id');
+					if(dragObjects.indexOf(position)<=-1)
+						dragObjects.push(position);	
+									
+					var item = col.cloneNode(true);		
+					if(dropObjects.indexOf(item)<=-1)
+						dropObjects.push(item);
+
 					if(event.preventDefault)
 						event.preventDefault();
+
 					return false;
 				},false);	
 	});
 };
 
 var addDropBehavior = function(){	
+	var curColumn = null;
 	var cols = document.querySelectorAll("#right_col");
 	[].forEach.call(cols, function(col) { 
 		console.log(col);	
+		curColumn = col;
 		col.addEventListener("dragenter", cancelDefault, false);
 		col.addEventListener("dragover", cancelDefault, false);
 		col.addEventListener("drop", handleDrop, false);
@@ -57,6 +73,22 @@ var addDropBehavior = function(){
 		if (e.stopPropagation)
 		{
 			e.stopPropagation(); // Stops some browsers from redirecting.
+		}
+
+		//Delete dragged node
+		if(dragObjects.length>0){	
+
+			var draggedNodeId = dragObjects.pop();
+			var childNode = document.getElementById(draggedNodeId);			   
+			var parentNode = childNode.parentNode;
+
+			console.log(parentNode);
+			childNode.parentNode.removeChild(childNode);	
+
+			
+			//Append dragged Node to parent			
+			curColumn.appendChild(dropObjects.pop());	
+			dropObjects = [];			
 		}
 		return false;
 	}
